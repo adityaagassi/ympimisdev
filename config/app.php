39bc91811311273dc2738b64cbbbd4a2ac74af01
Aditya Agassi <adityaagassi@gmail.com>
2019-01-09 12:44:08 +0700

    gak jelas

--- config/app.php
+++ config/app.php
@@ -231,7 +231,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\Datatables\Facades\DataTables::class,
         // 'Debugbar' => Barryvdh\Debugbar\Facade::class,
-
     ],
 
 ];
