185c9660404df04e3ce4791eb2a8c077cdc664c7
Aditya Agassi <adityaagassi@gmail.com>
2019-01-25 15:10:41 +0700

    new plan

--- app/Console/Commands/PlanStamps.php
+++ app/Console/Commands/PlanStamps.php
@@ -41,62 +41,68 @@ class PlanStamps extends Command
     public function handle()
     {
         $first = date('Y-m-01');
-
         $now = date('Y-m-d');
+        
+        $target = DB::table('production_schedules')
+        ->leftJoin('materials', 'materials.material_number', '=', 'production_schedules.material_number')
+        ->where('production_schedules.due_date', '=', $now)
+        ->where('materials.category', '=', 'FG');
+        $stock = DB::table('stamp_inventories');
+
+        $targetFL = $target->where('origin_group_code', '=', '041')->sum('production_schedules.quantity');
+        $stockFL = $stock->where('origin_group_code', '=', '041')->sum('stamp_inventories.quantity');
+
+        $dayFL = floor($stockFL/$targetFL)-1;
+        $addFL = ($stockFL/$targetFL)-$dayFL;
 
 
         if(date('D')=='Fri' || date('D')=='Wed' || date('D')=='Thu' || date('D')=='Sat'){
-            $h4 = date('Y-m-d', strtotime(carbon::now()->addDays(5)));
+            $hFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL+2)));
+            $aFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL+3)));
         }
         elseif(date('D')=='Sun'){
-            $h4 = date('Y-m-d', strtotime(carbon::now()->addDays(4)));
+            $hFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL+1)));
+            $aFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL+2)));
         }
         else{
-            $h4 = date('Y-m-d', strtotime(carbon::now()->addDays(3)));
+            $hFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL)));
+            $aFL = date('Y-m-d', strtotime(carbon::now()->addDays($dayFL+1)));
         }
 
-        // if(date('D')=='Thu'){
-        //     $h5 = date('Y-m-d', strtotime(carbon::now()->addDays(6)));
-        // }
-        // elseif(date('D')=='Fri'){
-        //     $h5 = date('Y-m-d', strtotime(carbon::now()->addDays(5)));
-        // }
-        // elseif(date('D')=='Sat'){
-        //     $h5 = date('Y-m-d', strtotime(carbon::now()->addDays(4)));
-        // }
-        // else{
-        //     $h5 = date('Y-m-d', strtotime(carbon::now()->addDays(3)));
-        // }
-
-
-        $query = "select model, '" . $now . "' as due_date, sum(plan) as plan from
+        $queryFL = "select model, '" . $now . "' as due_date, sum(plan) as plan from
         (
-            select materials.model, sum(plan) as plan from
-            (
-                select material_number, quantity as plan
-                from production_schedules 
-                where due_date >= '" . $first . "' and due_date <= '" . $h4 . "'
+        select materials.model, sum(plan) as plan from
+        (
+        select material_number, quantity as plan
+        from production_schedules 
+        where due_date >= '" . $first . "' and due_date <= '" . $hFL . "'
 
-                union all
+        union all
 
-                select material_number, -(quantity) as plan
-                from flo_details
-                where date(created_at) >= '" . $first . "' and date(created_at) <= '" . $h4 . "'
-            ) as plan
-            left join materials on materials.material_number = plan.material_number
-            group by materials.model
+        select material_number, round(quantity*".$addFL.") as plan
+        from production_schedules 
+        where due_date = '" . $aFL . "'
 
-            union all
+        union all
 
-            select model, -(quantity) as plan
-            from stamp_inventories
+        select material_number, -(quantity) as plan
+        from flo_details
+        where date(created_at) >= '" . $first . "' and date(created_at) <= '" . $hFL . "'
+        ) as plan
+        left join materials on materials.material_number = plan.material_number
+        group by materials.model
+
+        union all
+
+        select model, -(quantity) as plan
+        from stamp_inventories
         ) as result
         group by model, due_date    
-        having plan > 0";
+        having plan > 0 and model like 'YFL%'";
 
-        $planData = DB::select($query);
+        $planFL = DB::select($queryFL);
 
-        foreach ($planData as $row) {
+        foreach ($planFL as $row) {
             $model = $row->model;
             $due_date = $row->due_date;
             $quantity = $row->plan;
