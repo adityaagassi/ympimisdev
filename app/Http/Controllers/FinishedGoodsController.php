74629ace2ab98de655a211ed64df5eca3fbbe1a6
Aditya Agassi <adityaagassi@gmail.com>
2019-01-28 09:02:35 +0700

    fix stock

--- app/Http/Controllers/FinishedGoodsController.php
+++ app/Http/Controllers/FinishedGoodsController.php
@@ -384,7 +384,7 @@ class FinishedGoodsController extends Controller
 	}
 
 	public function fetch_fg_stock(){
-		$stock = DB::table('flos')
+		$stock2 = DB::table('flos')
 		->leftJoin('shipment_schedules', 'shipment_schedules.id', '=', 'flos.shipment_schedule_id')
 		->leftJoin('destinations', 'shipment_schedules.destination_code', '=', 'destinations.destination_code')
 		->leftJoin('material_volumes', 'material_volumes.material_number', '=', 'shipment_schedules.material_number')
@@ -396,7 +396,7 @@ class FinishedGoodsController extends Controller
 
 		// $jsonData = $stock->select('destinations.destination_shortname as destination', DB::raw('if(flos.status = 0, "Production", if(flos.status = 1, "InTransit", "FSTK")) as location'), DB::raw('sum(flos.actual) as actual'))->groupBy('destinations.destination_shortname', DB::raw('if(flos.status = 0, "Production", if(flos.status = 1, "InTransit", "FSTK"))'))->orderBy(DB::raw('field(location, "Production", "InTransit", "FSTK")'))->get();
 
-		$jsonData = $stock->select(db::raw('if(destinations.destination_shortname is null, "Maedaoshi", destinations.destination_shortname) as destination'), DB::raw('sum(if(flos.status = "0" or flos.status = "M", flos.actual, 0)) as production'), DB::raw('sum(if(flos.status = "1", flos.actual, 0)) as intransit'), DB::raw('sum(if(flos.status = "2", flos.actual, 0)) as fstk'), DB::raw('sum(flos.actual) as actual'))
+		$jsonData = $stock2->select(db::raw('if(destinations.destination_shortname is null, "Maedaoshi", destinations.destination_shortname) as destination'), DB::raw('sum(if(flos.status = "0" or flos.status = "M", flos.actual, 0)) as production'), DB::raw('sum(if(flos.status = "1", flos.actual, 0)) as intransit'), DB::raw('sum(if(flos.status = "2", flos.actual, 0)) as fstk'), DB::raw('sum(flos.actual) as actual'))
 		->groupBy('destinations.destination_shortname')->orderBy(db::raw('actual'), 'desc')->get();
 
 		$stock = DB::table('flos')
