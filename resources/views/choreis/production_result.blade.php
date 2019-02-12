015fbb7749463453530cb927e875d4931e859d4e
Aditya Agassi <adityaagassi@gmail.com>
2019-02-12 13:47:22 +0700

    select orther date

--- resources/views/choreis/production_result.blade.php
+++ resources/views/choreis/production_result.blade.php
@@ -78,19 +78,28 @@ table.table-bordered > tfoot > tr > th{
 		</div>
 	</div>
 	<div class="col-xs-2">
-		<div class="form-group">
-			<div class="input-group date">
-				<div class="input-group-addon bg-olive">
-					<i class="fa fa-calendar"></i>
+		<div class="row">
+			<span class="text-red"><i class="fa fa-info-circle"></i> Select Other Date</span>
+		</div>
+		<div class="row">
+			<div class="col-xs-7">
+				<div class="row">
+					<div class="input-group date">
+						<div class="input-group-addon bg-olive">
+							<i class="fa fa-calendar"></i>
+						</div>
+						<input type="text" class="form-control pull-right" id="datepicker" name="datepicker">
+					</div>
 				</div>
-				<input type="text" class="form-control pull-right" id="datepicker" name="datepicker">
+			</div>
+			<div class="col-xs-5">
+				<button id="search" onClick="searchDate()" class="btn bg-olive">Search</button>
 			</div>
 		</div>
 	</div>
-	<div class="col-xs-2">
+	{{-- <div class="col-xs-2">
 		<span class="text-red"><i class="fa fa-info-circle"></i> Select Other Date</span>
-		<button id="search" onClick="searchDate()" class="btn btn-sm pull-right bg-olive">Search</button>
-	</div>
+	</div> --}}
 	<div class="row">
 		<input type="hidden" name="dateHidden" value="{{ date('Y-m-d') }}" id="dateHidden">
 		<div class="col-md-12">
