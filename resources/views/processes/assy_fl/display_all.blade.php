2f877f21c14042d3fb5092ca28cbf0b0c2f6af02
Aditya Agassi <adityaagassi@gmail.com>
2019-03-22 09:06:32 +0700

    fix bugugugs

--- resources/views/processes/assy_fl/display_all.blade.php
+++ resources/views/processes/assy_fl/display_all.blade.php
@@ -19,173 +19,177 @@
 	table.table-bordered > tfoot > tr > th{
 		border:1px solid black;
 	}
-</style>
-@stop
-@section('header')
-<section class="content-header">
-	<h1>
-		Display WIP Flute<span class="text-purple"> FL仕掛品表示</span>
-		<small>Daily WIP <span class="text-purple"> 本日の仕掛品</span></small>
-	</h1>
-	<ol class="breadcrumb" id="last_update">
-	</ol>
-</section>
-@stop
-@section('content')
-<meta name="csrf-token" content="{{ csrf_token() }}">
-<section class="content">
-	<div class="row">
-		<div class="col-md-8">
-			<div id="container" style="width: 100%; height: 300; margin: 0 auto;"></div>
+
+	/*.dataTable > thead > tr > th[class*="sort"]:after{
+		content: "" !important;
+		}*/
+	</style>
+	@stop
+	@section('header')
+	<section class="content-header">
+		<h1>
+			Display WIP Flute<span class="text-purple"> FL仕掛品表示</span>
+			<small>Daily WIP <span class="text-purple"> 本日の仕掛品</span></small>
+		</h1>
+		<ol class="breadcrumb" id="last_update">
+		</ol>
+	</section>
+	@stop
+	@section('content')
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<section class="content">
+		<div class="row">
+			<div class="col-md-8">
+				<div id="container" style="width: 100%; height: 300; margin: 0 auto;"></div>
+			</div>
+			<div class="col-md-4">
+				<div id="container2" style="width: 100%; margin: 0 auto;"></div>
+			</div>
+			<div class="col-md-12">
+				<table id="tableStock" class="table table-bordered">
+				</table>
+			</div>
 		</div>
-		<div class="col-md-4">
-			<div id="container2" style="width: 100%; margin: 0 auto;"></div>
-		</div>
-		<div class="col-md-12">
-			<table id="tableStock" class="table table-bordered">
-			</table>
-		</div>
-	</div>
-</section>
+	</section>
 
-@endsection
-@section('scripts')
-<script src="{{ url("js/jquery.gritter.min.js") }}"></script>
-<script src="{{ url("js/highcharts.js")}}"></script>
-{{-- <script src="{{ url("js/highstock.js")}}"></script> --}}
-<script src="{{ url("js/exporting.js")}}"></script>
-<script src="{{ url("js/export-data.js")}}"></script>
-<script src="{{ url("js/bootstrap-toggle.min.js") }}"></script>
+	@endsection
+	@section('scripts')
+	<script src="{{ url("js/jquery.gritter.min.js") }}"></script>
+	<script src="{{ url("js/highcharts.js")}}"></script>
+	{{-- <script src="{{ url("js/highstock.js")}}"></script> --}}
+	<script src="{{ url("js/exporting.js")}}"></script>
+	<script src="{{ url("js/export-data.js")}}"></script>
+	<script src="{{ url("js/bootstrap-toggle.min.js") }}"></script>
 
-<script src="{{ url("js/dataTables.buttons.min.js")}}"></script>
-<script src="{{ url("js/buttons.flash.min.js")}}"></script>
-<script src="{{ url("js/jszip.min.js")}}"></script>
-{{-- <script src="{{ url("js/pdfmake.min.js")}}"></script> --}}
-<script src="{{ url("js/vfs_fonts.js")}}"></script>
-<script src="{{ url("js/buttons.html5.min.js")}}"></script>
-<script src="{{ url("js/buttons.print.min.js")}}"></script>
-<script>
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	<script src="{{ url("js/dataTables.buttons.min.js")}}"></script>
+	<script src="{{ url("js/buttons.flash.min.js")}}"></script>
+	<script src="{{ url("js/jszip.min.js")}}"></script>
+	{{-- <script src="{{ url("js/pdfmake.min.js")}}"></script> --}}
+	<script src="{{ url("js/vfs_fonts.js")}}"></script>
+	<script src="{{ url("js/buttons.html5.min.js")}}"></script>
+	<script src="{{ url("js/buttons.print.min.js")}}"></script>
+	<script>
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+			}
+		});
+
+		jQuery(document).ready(function() {
+			$('body').toggleClass("sidebar-collapse");
+			fetchTableStock();
+			fetchChartStock();
+		});
+
+		function addZero(i) {
+			if (i < 10) {
+				i = "0" + i;
+			}
+			return i;
 		}
-	});
 
-	jQuery(document).ready(function() {
-		$('body').toggleClass("sidebar-collapse");
-		fetchTableStock();
-		fetchChartStock();
-	});
-
-	function addZero(i) {
-		if (i < 10) {
-			i = "0" + i;
+		function getActualFullDate() {
+			var d = new Date();
+			var day = addZero(d.getDate());
+			var month = addZero(d.getMonth()+1);
+			var year = addZero(d.getFullYear());
+			var h = addZero(d.getHours());
+			var m = addZero(d.getMinutes());
+			var s = addZero(d.getSeconds());
+			return day + "-" + month + "-" + year + " (" + h + ":" + m + ":" + s +")";
 		}
-		return i;
-	}
 
-	function getActualFullDate() {
-		var d = new Date();
-		var day = addZero(d.getDate());
-		var month = addZero(d.getMonth()+1);
-		var year = addZero(d.getFullYear());
-		var h = addZero(d.getHours());
-		var m = addZero(d.getMinutes());
-		var s = addZero(d.getSeconds());
-		return day + "-" + month + "-" + year + " (" + h + ":" + m + ":" + s +")";
-	}
+		function fetchChartStock(){
+			var data = {
+				originGroupCode : '041'
+			}
+			$.get('{{ url("fetch/wipflallchart") }}', data, function(result, status, xhr){
+				console.log(status);
+				console.log(result);
+				console.log(xhr);
+				if(xhr.status == 200){
+					if(result.status){
+						$('#last_update').html('<b>Last Updated: '+ getActualFullDate() +'</b>');
+						var data = result.efficiencyData;
+						var stockCat = [];
+						var stockPlan = [];
+						var stockActual = [];
+						var maxPlan = [];
 
-	function fetchChartStock(){
-		var data = {
-			originGroupCode : '041'
-		}
-		$.get('{{ url("fetch/wipflallchart") }}', data, function(result, status, xhr){
-			console.log(status);
-			console.log(result);
-			console.log(xhr);
-			if(xhr.status == 200){
-				if(result.status){
-					$('#last_update').html('<b>Last Updated: '+ getActualFullDate() +'</b>');
-					var data = result.efficiencyData;
-					var stockCat = [];
-					var stockPlan = [];
-					var stockActual = [];
-					var maxPlan = [];
-
-					for (i = 0; i < data.length; i++) {
-						stockCat.push(data[i].model);
-						stockPlan.push(data[i].plan);
-						stockActual.push(data[i].stock);
-						maxPlan.push(data[i].max_plan);
-					}
-
-					var stockData = result.stockData;
-					var stock1 = '';
-					var stock2 = '';
-					var stock3 = '';
-					var stock4 = '';
-
-					for (x = 0; x < stockData.length; x++) {
-						if(stockData[x].process_code == 1){
-							stock1 = stockData[x].qty;
+						for (i = 0; i < data.length; i++) {
+							stockCat.push(data[i].model);
+							stockPlan.push(data[i].plan);
+							stockActual.push(data[i].stock);
+							maxPlan.push(data[i].max_plan);
 						}
-						if(stockData[x].process_code == 2){
-							stock2 = stockData[x].qty;
-						}
-						if(stockData[x].process_code == 3){
-							stock3 = stockData[x].qty;
-						}
-						if(stockData[x].process_code == 4){
-							stock4 = stockData[x].qty;
-						}
-					}
 
-					Highcharts.SVGRenderer.prototype.symbols['c-rect'] = function (x, y, w, h) {
-						return ['M', x, y + h / 2, 'L', x + w, y + h / 2];
-					};
+						var stockData = result.stockData;
+						var stock1 = '';
+						var stock2 = '';
+						var stock3 = '';
+						var stock4 = '';
 
-					Highcharts.chart('container', {
-						colors: ['rgba(248,161,63,1)','rgba(126,86,134,.9)','rgba(255,0,0,1)'],
-						chart: {
-							type: 'column',
-							backgroundColor: null,
-							spacingTop: 0,
-							spacingLeft: 0,
-							spacingRight: 0,
-							spacingBottom: 0
-						},
-						title: {
-							text: '<span>Current WIP Stock '+result.currStock+' Day(s)</span>',
-							style: {
-								fontSize: '30px',
-								fontWeight: 'bold'
+						for (x = 0; x < stockData.length; x++) {
+							if(stockData[x].process_code == 1){
+								stock1 = stockData[x].qty;
 							}
-						},
-						exporting: { enabled: false },
-						xAxis: {
-							tickInterval:  1,
-							overflow: true,
-							categories: stockCat,
-							labels:{
-								rotation: -45,
+							if(stockData[x].process_code == 2){
+								stock2 = stockData[x].qty;
+							}
+							if(stockData[x].process_code == 3){
+								stock3 = stockData[x].qty;
+							}
+							if(stockData[x].process_code == 4){
+								stock4 = stockData[x].qty;
+							}
+						}
+
+						Highcharts.SVGRenderer.prototype.symbols['c-rect'] = function (x, y, w, h) {
+							return ['M', x, y + h / 2, 'L', x + w, y + h / 2];
+						};
+
+						Highcharts.chart('container', {
+							colors: ['rgba(248,161,63,1)','rgba(126,86,134,.9)','rgba(255,0,0,1)'],
+							chart: {
+								type: 'column',
+								backgroundColor: null,
+								spacingTop: 0,
+								spacingLeft: 0,
+								spacingRight: 0,
+								spacingBottom: 0
 							},
-							min: 0					
-						},
-						yAxis: {
-							min: 1,
 							title: {
-								text: 'Set(s)'
+								text: '<span>Current WIP Stock '+result.currStock+' Day(s)</span>',
+								style: {
+									fontSize: '30px',
+									fontWeight: 'bold'
+								}
 							},
-							type:'logarithmic'
-						},
-						credits:{
-							enabled: false
-						},
-						legend: {
-							enabled: false
-						},
-						tooltip: {
-							shared: true
+							exporting: { enabled: false },
+							xAxis: {
+								tickInterval:  1,
+								overflow: true,
+								categories: stockCat,
+								labels:{
+									rotation: -45,
+								},
+								min: 0					
+							},
+							yAxis: {
+								min: 1,
+								title: {
+									text: 'Set(s)'
+								},
+								type:'logarithmic'
+							},
+							credits:{
+								enabled: false
+							},
+							legend: {
+								enabled: false
+							},
+							tooltip: {
+								shared: true
 							// enabled:true
 						},
 						plotOptions: {
@@ -224,78 +228,78 @@
 						}]
 					});
 
-					Highcharts.chart('container2', {
-						colors: ['rgb(241,92,128)','rgb(128,133,233)','rgb(247,163,92)','rgb(144,237,125)'],
-						chart: {
-							backgroundColor: null,
-							type: 'pie',
-							spacingTop: 0,
-							spacingLeft: 0,
-							spacingRight: 0,
-							spacingBottom: 0
-						},
-						exporting: { enabled: false },
-						title: {
-							text: null
-						},
-						tooltip: {
-							pointFormat: '{series.name}: <b>{point.y}</b>'
-						},
-						legend:{
-							enabled:false
-						},
-						plotOptions: {
-							pie: {
-								allowPointSelect: true,
-								cursor: 'pointer',
-								borderColor: 'rgb(126,86,134)',
-								dataLabels: {
-									enabled: true,
-									format: '<b>{point.name}<br/>{point.y} sets</b>',
-									distance: -50,
-									style:{
-										fontSize:'16px',
-										textOutline:0
-									},
-									color:'black',
-								},
-								showInLegend: true
+						Highcharts.chart('container2', {
+							colors: ['rgb(241,92,128)','rgb(128,133,233)','rgb(247,163,92)','rgb(144,237,125)'],
+							chart: {
+								backgroundColor: null,
+								type: 'pie',
+								spacingTop: 0,
+								spacingLeft: 0,
+								spacingRight: 0,
+								spacingBottom: 0
 							},
-							series:{
-								animation:{
-									duration:false
+							exporting: { enabled: false },
+							title: {
+								text: null
+							},
+							tooltip: {
+								pointFormat: '{series.name}: <b>{point.y}</b>'
+							},
+							legend:{
+								enabled:false
+							},
+							plotOptions: {
+								pie: {
+									allowPointSelect: true,
+									cursor: 'pointer',
+									borderColor: 'rgb(126,86,134)',
+									dataLabels: {
+										enabled: true,
+										format: '<b>{point.name}<br/>{point.y} sets</b>',
+										distance: -50,
+										style:{
+											fontSize:'16px',
+											textOutline:0
+										},
+										color:'black',
+									},
+									showInLegend: true
+								},
+								series:{
+									animation:{
+										duration:false
+									}
 								}
-							}
-						},
-						credits:{
-							enabled: false
-						},
-						series: [{
-							data: [{
-								name: 'Stamp-Kariawase',
-								y: stock1
-							}, {
-								name: 'Tanpoawase',
-								y: stock2
-							}, {
-								name: 'Yuge-Kanggou',
-								y: stock3
-							}, {
-								name: 'Chousei',
-								y: stock4
+							},
+							credits:{
+								enabled: false
+							},
+							series: [{
+								data: [{
+									name: 'Stamp-Kariawase',
+									y: stock1
+								}, {
+									name: 'Tanpoawase',
+									y: stock2
+								}, {
+									name: 'Yuge-Kanggou',
+									y: stock3
+								}, {
+									name: 'Chousei',
+									y: stock4
+								}]
 							}]
-						}]
-					});
-					setTimeout(fetchChartStock, 1000);
+						});
+						setTimeout(fetchChartStock, 1000);
+					}
+					else{
+						alert('Attempt to retrieve data failed')
+					}
 				}
 				else{
-					alert('Attempt to retrieve data failed')
+					alert('Disconnected from server')
 				}
-			}
-			else{
-				alert('Disconnected from server')
-			}
-		});
+			});
 
 }
 
@@ -434,6 +438,7 @@ function fetchTableStock(){
 							}
 						}
 					});
+				$('#tableStock').find("thead th").removeClass("sorting_asc");
 				setTimeout(fetchTableStock, 1000);
 			}
 			else{
