1650318da252ab86ca9a0061cbcba580bd1c4cb6
Aditya Agassi <adityaagassi@gmail.com>
2019-04-18 14:59:34 +0700

    fix

--- app/Flo.php
+++ app/Flo.php
@@ -23,6 +23,11 @@ class Flo extends Model
     	return $this->belongsTo('App\ShipmentSchedule', 'shipment_schedule_id')->withTrashed();
     }
 
+    public function material()
+    {
+        return $this->belongsTo('App\Material', 'material_number', 'material_number')->withTrashed();
+    }
+
     public function status()
     {
         return $this->belongsTo('App\Status', 'status', 'status_code')->withTrashed();
