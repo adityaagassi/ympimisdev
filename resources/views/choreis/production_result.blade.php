dc0d1b03fa84dc13f05838a0494cf7d7a6aa61e6
Aditya Agassi <adityaagassi@gmail.com>
2019-04-24 07:46:22 +0700

    minor loading

--- resources/views/choreis/production_result.blade.php
+++ resources/views/choreis/production_result.blade.php
@@ -146,70 +146,70 @@
 				<h4 class="modal-title" id="modalResultTitle"></h4>
 				<div class="modal-body table-responsive no-padding" style="min-height: 100px">
 					<center>
-					<i class="fa fa-spinner fa-spin" id="loading" style="font-size: 80px;"></i></center>
-					<table class="table table-hover table-bordered table-striped" id="tableResult">
-						<thead style="background-color: rgba(126,86,134,.7);">
-							<tr>
-								<th>Material</th>
-								<th>Description</th>
-								<th>Quantity</th>
-							</tr>
-						</thead>
-						<tbody id="modalResultBody">
-						</tbody>
-						<tfoot style="background-color: RGB(252, 248, 227);">
-							<th>Total</th>
-							<th></th>
-							<th id="modalResultTotal"></th>
-						</tfoot>
-					</table>
+						<i class="fa fa-spinner fa-spin" id="loading" style="font-size: 80px;"></i></center>
+						<table class="table table-hover table-bordered table-striped" id="tableResult">
+							<thead style="background-color: rgba(126,86,134,.7);">
+								<tr>
+									<th>Material</th>
+									<th>Description</th>
+									<th>Quantity</th>
+								</tr>
+							</thead>
+							<tbody id="modalResultBody">
+							</tbody>
+							<tfoot style="background-color: RGB(252, 248, 227);">
+								<th>Total</th>
+								<th></th>
+								<th id="modalResultTotal"></th>
+							</tfoot>
+						</table>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 
 
-@endsection
-@section('scripts')
-<script src="{{ url("js/highcharts.js")}}"></script>
-<script src="{{ url("js/exporting.js")}}"></script>
-<script src="{{ url("js/export-data.js")}}"></script>
-{{-- <script src="{{ url("js/jquery-ui.js")}}"></script> --}}
-<script>
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-		}
-	});
-
-	jQuery(document).ready(function() {
-		$('#datepicker').datepicker({
-			autoclose: true,
-			todayHighlight: true
+	@endsection
+	@section('scripts')
+	<script src="{{ url("js/highcharts.js")}}"></script>
+	<script src="{{ url("js/exporting.js")}}"></script>
+	<script src="{{ url("js/export-data.js")}}"></script>
+	{{-- <script src="{{ url("js/jquery-ui.js")}}"></script> --}}
+	<script>
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+			}
 		});
-		$('body').toggleClass("sidebar-collapse");
-		fillWeek();
-		fillDate();
-		fillChart($('#dateHidden').val());
-		setInterval(function(){
-			fillChart($('#dateHidden').val());
-		}, 10000);
-	});
 
-	$(function() {
-		$(document).keydown(function(e) {
-			switch(e.which) {
-				case 49:
-				$("#tab_header_1").click()
-				break;
-				case 50:
-				$("#tab_header_2").click()
-				break;
-				case 51:
-				$("#tab_header_3").click()
-				break;
+		jQuery(document).ready(function() {
+			$('#datepicker').datepicker({
+				autoclose: true,
+				todayHighlight: true
+			});
+			$('body').toggleClass("sidebar-collapse");
+			fillWeek();
+			fillDate();
+			fillChart($('#dateHidden').val());
+			setInterval(function(){
+				fillChart($('#dateHidden').val());
+			}, 10000);
+		});
+
+		$(function() {
+			$(document).keydown(function(e) {
+				switch(e.which) {
+					case 49:
+					$("#tab_header_1").click()
+					break;
+					case 50:
+					$("#tab_header_2").click()
+					break;
+					case 51:
+					$("#tab_header_3").click()
+					break;
 				// case 52:
 				// $("#tab_header_4").click()
 				// break;
@@ -221,131 +221,131 @@
 				break;
 			}
 		});
-	});
-
-	function searchDate(){
-		$.date = function(dateObject) {
-			var d = new Date(dateObject);
-			var day = d.getDate();
-			var month = d.getMonth() + 1;
-			var year = d.getFullYear();
-			if (day < 10) {
-				day = "0" + day;
-			}
-			if (month < 10) {
-				month = "0" + month;
-			}
-			var date = year + "-" + month + "-" + day;
-
-			return date;
-		};
-
-
-		var date = $.date($('#datepicker').val());
-
-		if($('#datepicker').val() != 0){
-			fillChart(date);
-		}
-	}
-
-	function fillWeek(){
-		$.get('{{ url("fetch/daily_production_result_week") }}', function(result, status, xhr){
-			console.log(status);
-			console.log(result);
-			console.log(xhr);
-			if(xhr.status == 200){
-				if(result.status){
-					$('#weekResult').html('');
-					var weekData = '';
-					$.each(result.weekData, function(key, value) {
-						weekData += '<button type="button" class="btn bg-purple btn-lg" id="' + value.week_name + '" onClick="fillDate(id)">' + value.week + '</button>&nbsp;';
-					});
-					$('#weekResult').append(weekData);
-				}
-				else{
-					alert('Attempt to retrieve data failed');
-				}
-			}
-			else{
-				alert('Disconnected from server');
-			}
 		});
-	}
 
-	function fillDate(id){
-		$("#weekResult .btn").removeClass("btn-active");
-		$("#"+id+"").addClass("btn-active");
-		var data = {
-			week:id,
+		function searchDate(){
+			$.date = function(dateObject) {
+				var d = new Date(dateObject);
+				var day = d.getDate();
+				var month = d.getMonth() + 1;
+				var year = d.getFullYear();
+				if (day < 10) {
+					day = "0" + day;
+				}
+				if (month < 10) {
+					month = "0" + month;
+				}
+				var date = year + "-" + month + "-" + day;
+
+				return date;
+			};
+
+
+			var date = $.date($('#datepicker').val());
+
+			if($('#datepicker').val() != 0){
+				fillChart(date);
+			}
 		}
-		$.get('{{ url("fetch/daily_production_result_date") }}', data, function(result, status, xhr){
-			console.log(status);
-			console.log(result);
-			console.log(xhr);
-			if(xhr.status == 200){
-				if(result.status){
-					$('#dateResult').html('');
-					var dateData = '';
-					$.each(result.dateData, function(key, value) {
-						dateData += '<button type="button" class="btn bg-olive" id="' + value.week_date + '" onClick="fillChart(id)">' + value.week_date_name + '</button>&nbsp;';
-					});
-					$('#dateResult').append(dateData);
+
+		function fillWeek(){
+			$.get('{{ url("fetch/daily_production_result_week") }}', function(result, status, xhr){
+				console.log(status);
+				console.log(result);
+				console.log(xhr);
+				if(xhr.status == 200){
+					if(result.status){
+						$('#weekResult').html('');
+						var weekData = '';
+						$.each(result.weekData, function(key, value) {
+							weekData += '<button type="button" class="btn bg-purple btn-lg" id="' + value.week_name + '" onClick="fillDate(id)">' + value.week + '</button>&nbsp;';
+						});
+						$('#weekResult').append(weekData);
+					}
+					else{
+						alert('Attempt to retrieve data failed');
+					}
 				}
 				else{
-					alert('Attempt to retrieve data failed');
+					alert('Disconnected from server');
 				}
+			});
+		}
+
+		function fillDate(id){
+			$("#weekResult .btn").removeClass("btn-active");
+			$("#"+id+"").addClass("btn-active");
+			var data = {
+				week:id,
 			}
-			else{
-				alert('Disconnected from server');
-			}			
-		});		
-	}
-
-	function addZero(i) {
-		if (i < 10) {
-			i = "0" + i;
+			$.get('{{ url("fetch/daily_production_result_date") }}', data, function(result, status, xhr){
+				console.log(status);
+				console.log(result);
+				console.log(xhr);
+				if(xhr.status == 200){
+					if(result.status){
+						$('#dateResult').html('');
+						var dateData = '';
+						$.each(result.dateData, function(key, value) {
+							dateData += '<button type="button" class="btn bg-olive" id="' + value.week_date + '" onClick="fillChart(id)">' + value.week_date_name + '</button>&nbsp;';
+						});
+						$('#dateResult').append(dateData);
+					}
+					else{
+						alert('Attempt to retrieve data failed');
+					}
+				}
+				else{
+					alert('Disconnected from server');
+				}			
+			});		
 		}
-		return i;
-	}
 
-	function getActualFullDate() {
-		var d = new Date();
-		var day = addZero(d.getDate());
-		var month = addZero(d.getMonth()+1);
-		var year = addZero(d.getFullYear());
-		var h = addZero(d.getHours());
-		var m = addZero(d.getMinutes());
-		var s = addZero(d.getSeconds());
-		return day + "-" + month + "-" + year + " (" + h + ":" + m + ":" + s +")";
-	}
-
-	function fillChart(id){
-		if(id != 0){
-			$('#dateHidden').val(id);
+		function addZero(i) {
+			if (i < 10) {
+				i = "0" + i;
+			}
+			return i;
 		}
-		var date = id;
-		var data = {
-			date:date,
-		};
-		$.get('{{ url("fetch/daily_production_result") }}', data, function(result, status, xhr){
-			console.log(status);
-			console.log(result);
-			console.log(xhr);
-			if(xhr.status == 200){
-				if(result.status){
-					$('#last_update').html('<b>Last Updated: '+ getActualFullDate() +'</b>');
-					var data = result.chartResult1;
-					var xAxis = []
-					, planCount = []
-					, actualCount = []
-					, xAxisEI = []
-					, planCountEI = []
-					, actualCountEI = []
 
-					$("#dateResult .btn").removeClass("btn-active");
-					$("#"+date+"").addClass("btn-active");
+		function getActualFullDate() {
+			var d = new Date();
+			var day = addZero(d.getDate());
+			var month = addZero(d.getMonth()+1);
+			var year = addZero(d.getFullYear());
+			var h = addZero(d.getHours());
+			var m = addZero(d.getMinutes());
+			var s = addZero(d.getSeconds());
+			return day + "-" + month + "-" + year + " (" + h + ":" + m + ":" + s +")";
+		}
 
-					for (i = 0; i < data.length; i++) {
+		function fillChart(id){
+			if(id != 0){
+				$('#dateHidden').val(id);
+			}
+			var date = id;
+			var data = {
+				date:date,
+			};
+			$.get('{{ url("fetch/daily_production_result") }}', data, function(result, status, xhr){
+				console.log(status);
+				console.log(result);
+				console.log(xhr);
+				if(xhr.status == 200){
+					if(result.status){
+						$('#last_update').html('<b>Last Updated: '+ getActualFullDate() +'</b>');
+						var data = result.chartResult1;
+						var xAxis = []
+						, planCount = []
+						, actualCount = []
+						, xAxisEI = []
+						, planCountEI = []
+						, actualCountEI = []
+
+						$("#dateResult .btn").removeClass("btn-active");
+						$("#"+date+"").addClass("btn-active");
+
+						for (i = 0; i < data.length; i++) {
 						// if(jQuery.inArray(data[i].hpl, ['CLFG', 'ASFG', 'TSFG', 'FLFG']) !== -1){
 							xAxis.push(data[i].hpl);
 							planCount.push(data[i].plan);
@@ -1056,6 +1056,10 @@ function modalResult(hpl, name, now, first, last){
 }
 
 function modalAccuracy(hpl, name, now, first, last){
+	$('#modalResult').modal('show');
+	$('#loading').show();
+	$('#modalResultTitle').hide();
+	$('#tableResult').hide();
 	var data = {
 		hpl:hpl,
 		name:name,
@@ -1095,7 +1099,10 @@ function modalAccuracy(hpl, name, now, first, last){
 				$('#modalResultBody').append(accuracyData);
 				$('#modalResultTotal').html('');
 				$('#modalResultTotal').append(accuracyTotal.toLocaleString());
-				$('#modalResult').modal('show');
+
+				$('#loading').hide();
+				$('#modalResultTitle').show();
+				$('#tableResult').show();
 			}
 			else{
 				alert('Attempt to retrieve data failed');
@@ -1108,6 +1115,10 @@ function modalAccuracy(hpl, name, now, first, last){
 }
 
 function modalBL(hpl, name, week, date){
+	$('#modalResult').modal('show');
+	$('#loading').show();
+	$('#modalResultTitle').hide();
+	$('#tableResult').hide();
 	var data = {
 		hpl:hpl,
 		name:name,
@@ -1136,7 +1147,10 @@ function modalBL(hpl, name, week, date){
 				$('#modalResultBody').append(blData);
 				$('#modalResultTotal').html('');
 				$('#modalResultTotal').append(blTotal.toLocaleString());
-				$('#modalResult').modal('show');
+				
+				$('#loading').hide();
+				$('#modalResultTitle').show();
+				$('#tableResult').show();
 			}
 			else{
 				alert('Attempt to retrieve data failed');
