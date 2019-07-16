bb860c64b23e682628833efd25979e14c90ae6bc
Aditya Agassi <adityaagassi@gmail.com>
2019-07-16 11:15:04 +0700

    asdas

--- resources/views/layouts/display_backup.blade.php
+++ resources/views/layouts/display_backup.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <link rel="shortcut icon" type="image/x-icon" href="{{ url("logo_mirai.png")}}" />
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,76 +18,34 @@
   <link rel="stylesheet" href="{{ url("dist/css/skins/skin-purple.css")}}">
   <link rel="stylesheet" href="{{ url("fonts/SourceSansPro.css")}}">
   <link rel="stylesheet" href="{{ url("css/buttons.dataTables.min.css")}}">
-
+  
   {{-- <link rel="stylesheet" href="{{ url("plugins/pace/pace.min.css")}}"> --}}
   @yield('stylesheets')
 </head>
 <body class="hold-transition skin-purple layout-top-nav">
   <div class="wrapper">
-    <header class="main-header" >
+    <header class="main-header pull-left"  style="width:100%">
       <nav class="navbar navbar-static-top">
-        {{-- <div class="container"> --}}
+        <div class="container"  style="width:100%">
           <div class="navbar-header">
             <a href="{{ url("/home") }}" class="logo">
               <span style="font-size: 35px"><img src="{{ url("images/logo_mirai_bundar.png")}}" height="45px" style="margin-bottom: 6px;">&nbsp;<b>M I R A I</b></span>
             </a>
           </div>
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Service Desk <span class="caret"></span></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="{{ url("index/wjo_mis") }}">Management Information System <b>(MIS)</b></a></li>
-                </ul>
-              </li>
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  @php
-                  $avatar = 'images/avatar/'.Auth::user()->avatar;
-                  @endphp
-                  <img src="{{ url($avatar) }}" class="user-image" alt="User Image">
-                  <span class="hidden-xs">{{Auth::user()->name}}</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <li class="user-header">
-                    <img src="{{ url($avatar) }}" class="img-circle" alt="User Image">
-                    <p>
-                      {{Auth::user()->name}}
-                      <small>{{Auth::user()->email}}</small>
-                    </p>
-                  </li>
-                  <li class="user-footer">
-                    <div class="row">
-                      <div class="col-xs-4 pull-left">
-                        <a class="btn btn-info btn-flat" href="{{ url("setting/user") }}">Setting</a>
-                      </div>
-                      <div class="col-xs-4 pull-right">
-                        <a class="btn btn-danger btn-flat" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                          Logout
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                          {{ csrf_field() }}
-                        </form>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        {{-- </div> --}}
+        </div>
       </nav>
     </header>
-    <div class="content-wrapper" style="background-color: rgb(62,78,99);">
-      {{-- <div class="container"> --}}
+    <div class="content-wrapper" style="background-color: rgb(62,78,99); width: 100%" >
+      <div class="container" style="width:100%">
+
         <section class="content-header">
          @yield('header')
        </section>
-       <section class="content" style="padding-top:0px">
+       <section class="content">
          @yield('content')
        </section>
      </div>
-   {{-- </div> --}}
+   </div>
    @include('layouts.footer')
  </div>
  <script src="{{ url("bower_components/jquery/dist/jquery.min.js")}}"></script>
@@ -101,7 +60,6 @@
  {{-- <script src="{{ url("bower_components/PACE/pace.min.js")}}"></script> --}}
  <script src="{{ url("dist/js/adminlte.min.js")}}"></script>
  <script src="{{ url("dist/js/demo.js")}}"></script>
- {{-- <script>$(document).ajaxStart(function() { Pace.restart(); });</script> --}}
  @yield('scripts')
 </body>
 </html>
