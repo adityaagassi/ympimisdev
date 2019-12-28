b5dba8d6e3708cac11561386ca6a7361ca6c8ddf
Aditya Agassi <adityaagassi@gmail.com>
2019-12-28 08:19:12 +0700

    apd print

--- resources/views/apd_check/print_email.blade.php
+++ resources/views/apd_check/print_email.blade.php
@@ -50,9 +50,9 @@
 					<td style="border: 1px solid black;" colspan="10" class="head">PT. YAMAHA MUSICAL PRODUCTS INDONESIA</td>
 				</tr>
 				<tr>
-					<td colspan="2" class="head">Department</td>
-					<td colspan="3" class="head">{{ $departments }}</td>
-					<td class="head" rowspan="3" colspan="2" style="padding: 15px;vertical-align: middle"><center><b>{{ $activity_name }}</b></center></td>
+					<td class="head">Department</td>
+					<td class="head">{{ $departments }}</td>
+					<td class="head" rowspan="3" colspan="6" style="padding: 15px;vertical-align: middle"><center><b>{{ $activity_name }}</b></center></td>
 					<td class="head" rowspan="3"><center>Checked<br><br>
 						@if($jml_null == 0)
 							<b style='color:green'>Approved</b><br>
@@ -74,12 +74,12 @@
 					@endif
 				</tr>
 				<tr>
-					<td colspan="2" class="head">Sub Section</td>
-					<td colspan="3" class="head">{{ $subsection }}</td>
+					<td class="head">Sub Section</td>
+					<td class="head">{{ $subsection }}</td>
 				</tr>
 				<tr>
-					<td colspan="2" class="head">Month</td>
-					<td colspan="3" class="head">{{ $monthTitle }}</td>
+					<td class="head">Month</td>
+					<td class="head">{{ $monthTitle }}</td>
 				</tr>
 				<tr>
 					<td class="head"><center>Date</center></td>
@@ -87,7 +87,7 @@
 					<td class="head"><center>Proses</center></td>
 					<td class="head"><center>Jenis APD</center></td>
 					<td class="head"><center>Kondisi</center></td>
-					<td class="head" colspan="3"><center>Foto Aktual</center></td>
+					<td class="head" colspan="4"><center>Foto Aktual</center></td>
 					<td class="head"><center>Checked By</center></td>
 				</tr>
 				<form role="form" method="post" action="{{url('index/apd_check/approval/'.$id.'/'.$month)}}">
@@ -98,7 +98,7 @@
 					<td style="vertical-align: middle" class="head"><center>{{ $apd_check->proses }}</center></td>
 					<td style="vertical-align: middle" class="head"><center>{{ $apd_check->jenis_apd }}</center></td>
 					<td style="vertical-align: middle" class="head"><center>{{ $apd_check->kondisi }}</center></td>
-					<td style="vertical-align: middle" class="head" colspan="3"><center><?php echo  $apd_check->foto_aktual ?></center></td>
+					<td style="vertical-align: middle" class="head" colspan="4"><center><?php echo  $apd_check->foto_aktual ?></center></td>
 					<td style="vertical-align: middle" class="head"><center>{{ $apd_check->leader }}</center></td>
 					@if($jml_null > 0 && $role_code != 'M')
 					<td id="approval2" style="vertical-align: middle;">
