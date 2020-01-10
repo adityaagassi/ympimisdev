0b2ebb59122e86957733d49ff458f89b873d0373
Aditya Agassi <adityaagassi@gmail.com>
2020-01-10 07:55:55 +0700

    op rank

--- resources/views/processes/middle/display/buffing_op_ranking.blade.php
+++ resources/views/processes/middle/display/buffing_op_ranking.blade.php
@@ -80,7 +80,7 @@
 			<div class="col-xs-12" style="margin-top: 5px;">
 				<div id="container1" style="width: 100%; margin-top: 1%;"></div>
 				<div id="container3" style="width: 100%; margin-top: 1%;"></div>
-				<div id="container2" style="width: 100%; margin-top: 1%; display: none;"></div>
+				<div id="container2" style="width: 100%; margin-top: 1%;"></div>
 				
 			</div>
 		</div>
