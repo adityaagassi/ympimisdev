380283a9ba4656d950cfdc3d7c2250d8b6871c07
Aditya Agassi <adityaagassi@gmail.com>
2020-01-30 08:46:44 +0700

    add new user

--- app/Console/Commands/SyncSunfish.php
+++ app/Console/Commands/SyncSunfish.php
@@ -4,83 +4,106 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use App\User;
 
 class SyncSunfish extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'sync:sunfish';
+/**
+* The name and signature of the console command.
+*
+* @var string
+*/
+protected $signature = 'sync:sunfish';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+/**
+* The console command description.
+*
+* @var string
+*/
+protected $description = 'Command description';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
+/**
+* Create a new command instance.
+*
+* @return void
+*/
+public function __construct()
+{
+    parent::__construct();
+}
+
+/**
+* Execute the console command.
+*
+* @return mixed
+*/
+public function handle()
+{
+    $insert = array();
+    $datas = db::connection('sunfish')->select("SELECT DISTINCT * FROM VIEW_YMPI_Emp_OrgUnit WHERE Emp_no <> 'sunfish'");
+    $datas2 = json_decode(json_encode($datas), true);
+
+    foreach ($datas2 as $data) {
+        $row = array();
+
+        $row['employee_id'] = $data['Emp_no'];
+        $row['name'] = $data['Full_name'];
+        $row['gender'] = $data['gender'];
+        $row['birth_place'] = $data['birthplace'];
+        $row['birth_date'] = $data['birthdate'];
+        $row['address'] = $data['address'];
+        $row['phone'] = $data['phone'];
+        $row['card_id'] = $data['identity_no'];
+        $row['npwp'] = $data['taxfilenumber'];
+        $row['JP'] = $data['JP'];
+        $row['BPJS'] = $data['BPJS'];
+        $row['hire_date'] = $data['start_date'];
+        $row['end_date'] = $data['end_date'];
+        $row['position'] = $data['pos_name_en'];
+        $row['grade_code'] = $data['grade_code'];
+        $row['grade_name'] = $data['gradecategory_name'];
+        $row['division'] = $data['Division'];
+        $row['department'] = $data['Department'];
+        $row['section'] = $data['Section'];
+        $row['group'] = $data['Group'];
+        $row['sub_group'] = $data['Sub-Group'];
+        $row['group'] = $data['Group'];
+        $row['employment_status'] = $data['employ_code'];
+        $row['cost_center'] = $data['cost_center_code'];
+        $row['assignment'] = $data['Penugasan'];
+        $row['union'] = $data['Union'];
+        $row['created_at'] = date('Y-m-d H:i:s');
+        $row['updated_at'] = date('Y-m-d H:i:s');
+
+        $insert[] = $row;
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
+    DB::table('employee_syncs')->truncate();
+    foreach (array_chunk($insert,1000) as $t)  
     {
-        $insert = array();
-        $datas = db::connection('sunfish')->select("SELECT DISTINCT * FROM VIEW_YMPI_Emp_OrgUnit");
-        $datas2 = json_decode(json_encode($datas), true);
-        
-        foreach ($datas2 as $data) {
-            $row = array();
+        DB::table('employee_syncs')->insert($t);
+    }
 
-            $row['employee_id'] = $data['Emp_no'];
-            $row['name'] = $data['Full_name'];
-            $row['gender'] = $data['gender'];
-            $row['birth_place'] = $data['birthplace'];
-            $row['birth_date'] = $data['birthdate'];
-            $row['address'] = $data['address'];
-            $row['phone'] = $data['phone'];
-            $row['card_id'] = $data['identity_no'];
-            $row['npwp'] = $data['taxfilenumber'];
-            $row['JP'] = $data['JP'];
-            $row['BPJS'] = $data['BPJS'];
-            $row['hire_date'] = $data['start_date'];
-            $row['end_date'] = $data['end_date'];
-            $row['position'] = $data['pos_name_en'];
-            $row['grade_code'] = $data['grade_code'];
-            $row['grade_name'] = $data['gradecategory_name'];
-            $row['division'] = $data['Division'];
-            $row['department'] = $data['Department'];
-            $row['section'] = $data['Section'];
-            $row['group'] = $data['Group'];
-            $row['sub_group'] = $data['Sub-Group'];
-            $row['group'] = $data['Group'];
-            $row['employment_status'] = $data['employ_code'];
-            $row['cost_center'] = $data['cost_center_code'];
-            $row['assignment'] = $data['Penugasan'];
-            $row['union'] = $data['Union'];
-            $row['created_at'] = date('Y-m-d H:i:s');
-            $row['updated_at'] = date('Y-m-d H:i:s');
+    $users = DB::table('users')->get();
 
-            $insert[] = $row;
-        }
-        
-        DB::table('employee_syncs')->truncate();
-        foreach (array_chunk($insert,1000) as $t)  
-        {
-            DB::table('employee_syncs')->insert($t);
+    $usernames = array();
+    foreach ($users as $user) {
+        array_push($usernames, strtoupper($user->username));
+    }
+
+    foreach ($datas as $data) {     
+        if(!in_array($data->Emp_no, $usernames)){
+            $insert_user = New User([
+                'name' => ucwords($data->Full_name),
+                'email' => strtolower($data->Emp_no).'@gmail.com',
+                'password' => bcrypt('123456'),
+                'username' => strtolower($data->Emp_no),
+                'role_code' => 'emp-srv',
+                'avatar' => strtolower($data->Emp_no.'jpg'),
+                'created_by' => '1'
+            ]);
+            $insert_user->save();
         }
     }
+}
 }
