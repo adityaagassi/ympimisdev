419628585eba14efa42b13cc1c50143917dea8db
Aditya Agassi <adityaagassi@gmail.com>
2020-02-12 15:38:09 +0700

    wjo

--- app/Http/Controllers/WorkshopController.php
+++ app/Http/Controllers/WorkshopController.php
@@ -1328,21 +1328,18 @@ class WorkshopController extends Controller{
 	public function fetchWorkloadOperatorDetail(Request $request){
 		$name = $request->get('name');
 
-		$detail = db::select('select concat(SPLIT_STRING(emp.`name`, " ", 1)," ",SPLIT_STRING(emp.`name`, " ", 2)) as `name`, workload.order_no, wjo.target_date, tag.remark as tag_number, wjo.item_name, workload.workload from
-			(select wjo.operator, workload.order_no, round((workload.workload/60), 0) as workload from
-			(select workload.order_no, sum(workload.workload) workload from
-			(select flow.order_no, sum(std_time) as workload from workshop_flow_processes flow
+		$detail = db::select('select concat(SPLIT_STRING(emp.`name`, " ", 1)," ",SPLIT_STRING(emp.`name`, " ", 2)) as `name`, workload.order_no, wjo.target_date, tag.remark as tag_number, wjo.item_name, round((workload.workload/60), 0) as workload from			
+			(select workload.order_no, workload.operator, sum(workload.workload) workload from
+			(select flow.order_no, flow.operator, sum(std_time) as workload from workshop_flow_processes flow
 			left join workshop_job_orders wjo on wjo.order_no = flow.order_no
 			where wjo.remark < 4
-			group by flow.order_no
+			group by flow.order_no, flow.operator
 			union all
-			select log.order_no, -sum(TIMESTAMPDIFF(second,log.started_at,log.created_at)) as workload from workshop_logs log
+			select log.order_no, log.operator_id as operator, -sum(TIMESTAMPDIFF(second,log.started_at,log.created_at)) as workload from workshop_logs log
 			left join workshop_job_orders wjo on wjo.order_no = log.order_no
 			where wjo.remark < 4
-			group by log.order_no) as workload
-			group by workload.order_no) workload
-			left join workshop_job_orders wjo
-			on wjo.order_no = workload.order_no
+			group by log.order_no, log.operator_id) as workload
+			group by workload.order_no, workload.operator
 			having workload > 0) workload
 			left join employee_syncs emp
 			on emp.employee_id = workload.operator
