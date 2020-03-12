88322bf5dcc5e65165a665df31cc73bd1b1a07b3
Aditya Agassi <adityaagassi@gmail.com>
2020-03-12 08:16:27 +0700

    dx

--- app/Http/Controllers/PantryController.php
+++ app/Http/Controllers/PantryController.php
@@ -285,6 +285,7 @@ class PantryController extends Controller
         LEFT JOIN ympimis.employee_syncs ON ympimis.employee_syncs.employee_id = pantry_logs.employee_id 
         WHERE
         date( in_time ) = '".$date."'
+        AND round( TIMESTAMPDIFF( SECOND, pantry_logs.in_time, pantry_logs.out_time ) / 60, 2 ) > 0.1
         ".$where." 
         GROUP BY
         pantry_logs.employee_id,
