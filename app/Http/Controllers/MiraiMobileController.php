cdea20868f18d55119ceabde3c56adee8703e5b0
ympimis <ympimis@gamil.com>
2020-05-22 11:01:20 +0700

    tes

--- app/Http/Controllers/MiraiMobileController.php
+++ app/Http/Controllers/MiraiMobileController.php
@@ -390,7 +390,7 @@ class MiraiMobileController extends Controller
   public function fetchLocation()
   {
 
-    $employee_location = db::connection('mobile')->select("SELECT act.answer_date, employees.department, if(employees.department ='Management Information System', 0, count(act.employee_id)) as jumlah from
+    $employee_location = db::connection('mobile')->select("SELECT act.answer_date, employees.department, count(act.employee_id) as jumlah from
       (SELECT employee_id, `name`, answer_date, village, city, province FROM quiz_logs
       WHERE id IN (
       SELECT MIN(id)
@@ -398,8 +398,8 @@ class MiraiMobileController extends Controller
       GROUP BY employee_id, `name`, answer_date
       )) as act
       left join employees on employees.employee_id = act.employee_id
-      join (select employee_id, tanggal from groups where remark = 'OFF') all_groups on all_groups.employee_id = act.employee_id AND all_groups.tanggal = act.answer_date
-      where act.city <> employees.kota and answer_date >= '2020-04-13'
+      left join (select employee_id, tanggal from groups where remark = 'OFF') all_groups on all_groups.employee_id = act.employee_id AND all_groups.tanggal = act.answer_date
+      where act.city <> employees.kota and answer_date >= '2020-04-13' and employees.department <> 'Management Information System'
       and employees.department is not null
       group by employees.department, answer_date
       ");
@@ -417,12 +417,6 @@ class MiraiMobileController extends Controller
   public function fetchLocationDetail(Request $request)
   {
     if ($request->get('department') != "") {
-      $department = $request->get('department');
-
-      if($department == 'Management Information System'){
-        $department = 'mis';
-      }
-      
       $dept = "AND employees.department = '".$request->get('department')."'";
     }else{
       $dept = "";
@@ -493,7 +487,7 @@ class MiraiMobileController extends Controller
       left join employees on employees.employee_id = act.employee_id
       join (select employee_id, tanggal from groups where remark = 'OFF') all_groups on all_groups.employee_id = act.employee_id AND all_groups.tanggal = act.answer_date
       where act.city <> employees.kota and answer_date >= '2020-04-13'
-      and employees.department is not null and empliyees.department <> 'Management Information System'
+      and employees.department is not null and employees.department <> 'Management Information System'
       ");
 
     $data = array(
