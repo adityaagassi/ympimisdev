338d511729de79d353eee03e954e8de8ce543254
ympimis <ympimis@gamil.com>
2020-08-28 10:52:59 +0700

    chart style

--- resources/views/stocktakings/monthly/index.blade.php
+++ resources/views/stocktakings/monthly/index.blade.php
@@ -574,16 +574,11 @@
 					Highcharts.chart('container', {
 						chart: {
 							height: 225,
-							type: 'column',
-							backgroundColor: {
-								linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-								stops: []
-							},
+							type: 'column'
 						},
 						title: {
 							text: 'Progress Input PI Stocktaking',
 							style: {
-								fontSize: '1.5vw',
 								fontWeight: 'bold'
 							}
 						},	
@@ -595,14 +590,7 @@
 						},
 						xAxis: {
 							categories: location,
-							type: 'category',
-							gridLineWidth: 5,
-							gridLineColor: 'RGB(204,255,255)',
-							labels: {
-								style: {
-									fontSize: '1vw'
-								}
-							},
+							type: 'category'
 						},
 						yAxis: {
 							title: {
@@ -636,7 +624,6 @@
 										return this.y;
 									},
 									style: {
-										fontSize:'18px',
 										fontWeight: 'bold',
 									}
 								},
@@ -652,11 +639,11 @@
 						series: [{
 							name: 'Empty',
 							data: empty,
-							color: 'rgb(255,116,116)'
+							color: 'rgba(255, 0, 0, 0.25)'
 						}, {
 							name: 'Inputted',
 							data: fill,
-							color: 'rgb(144,238,126)'
+							color: '#00a65a'
 						}]
 					});
 				}
@@ -793,16 +780,11 @@
 					Highcharts.chart('container1', {
 						chart: {
 							height: 225,
-							type: 'column',
-							backgroundColor: {
-								linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-								stops: []
-							},
+							type: 'column'
 						},
 						title: {
 							text: 'Progress Audit 1 Stocktaking',
 							style: {
-								fontSize: '1.5vw',
 								fontWeight: 'bold'
 							}
 						},	
@@ -814,14 +796,7 @@
 						},
 						xAxis: {
 							categories: location,
-							type: 'category',
-							gridLineWidth: 5,
-							gridLineColor: 'RGB(204,255,255)',
-							labels: {
-								style: {
-									fontSize: '1vw'
-								}
-							},
+							type: 'category'
 						},
 						yAxis: {
 							title: {
@@ -855,7 +830,6 @@
 										return this.y;
 									},
 									style: {
-										fontSize:'18px',
 										fontWeight: 'bold',
 									}
 								},
@@ -871,11 +845,11 @@
 						series: [{
 							name: 'Not yet',
 							data: notyet,
-							color: 'rgb(255,116,116)'
+							color: 'rgba(255, 0, 0, 0.25)'
 						}, {
 							name: 'Audited',
 							data: audited,
-							color: 'rgb(144,238,126)'
+							color: '#00a65a'
 						}]
 					});
 				}
@@ -980,16 +954,11 @@
 					Highcharts.chart('container2', {
 						chart: {
 							height: 225,
-							type: 'column',
-							backgroundColor: {
-								linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
-								stops: []
-							},
+							type: 'column'
 						},
 						title: {
 							text: 'Quick Count Variance',
 							style: {
-								fontSize: '1.5vw',
 								fontWeight: 'bold'
 							}
 						},
@@ -1001,14 +970,7 @@
 						},
 						xAxis: {
 							categories: location,
-							type: 'category',
-							gridLineWidth: 5,
-							gridLineColor: 'RGB(204,255,255)',
-							labels: {
-								style: {
-									fontSize: '1vw'
-								}
-							},
+							type: 'category'
 						},
 						yAxis: {
 							title: {
@@ -1035,7 +997,6 @@
 									enabled: true,
 									format: '{point.y:.2f}%',
 									style: {
-										fontSize:'18px',
 										fontWeight: 'bold',
 									}
 								},
@@ -1051,7 +1012,7 @@
 						series: [{
 							name: 'Variance',
 							data: variance,
-							color: 'rgb(255,116,116)'
+							color: '#ff851b'
 						}]
 					});
 				}else{
@@ -1150,6 +1111,209 @@
 		});
 	}
 
+	Highcharts.createElement('link', {
+		href: '{{ url("fonts/UnicaOne.css")}}',
+		rel: 'stylesheet',
+		type: 'text/css'
+	}, null, document.getElementsByTagName('head')[0]);
+
+	Highcharts.theme = {
+		colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+		'#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+		chart: {
+			backgroundColor: {
+				linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+				stops: [
+				[0, '#2a2a2b'],
+				[1, '#3e3e40']
+				]
+			},
+			style: {
+				fontFamily: 'sans-serif'
+			},
+			plotBorderColor: '#606063'
+		},
+		title: {
+			style: {
+				color: '#E0E0E3',
+				textTransform: 'uppercase',
+				fontSize: '20px'
+			}
+		},
+		subtitle: {
+			style: {
+				color: '#E0E0E3',
+				textTransform: 'uppercase'
+			}
+		},
+		xAxis: {
+			gridLineColor: '#707073',
+			labels: {
+				style: {
+					color: '#E0E0E3'
+				}
+			},
+			lineColor: '#707073',
+			minorGridLineColor: '#505053',
+			tickColor: '#707073',
+			title: {
+				style: {
+					color: '#A0A0A3'
+
+				}
+			}
+		},
+		yAxis: {
+			gridLineColor: '#707073',
+			labels: {
+				style: {
+					color: '#E0E0E3'
+				}
+			},
+			lineColor: '#707073',
+			minorGridLineColor: '#505053',
+			tickColor: '#707073',
+			tickWidth: 1,
+			title: {
+				style: {
+					color: '#A0A0A3'
+				}
+			}
+		},
+		tooltip: {
+			backgroundColor: 'rgba(0, 0, 0, 0.85)',
+			style: {
+				color: '#F0F0F0'
+			}
+		},
+		plotOptions: {
+			series: {
+				dataLabels: {
+					color: 'white'
+				},
+				marker: {
+					lineColor: '#333'
+				}
+			},
+			boxplot: {
+				fillColor: '#505053'
+			},
+			candlestick: {
+				lineColor: 'white'
+			},
+			errorbar: {
+				color: 'white'
+			}
+		},
+		legend: {
+			itemStyle: {
+				color: '#E0E0E3'
+			},
+			itemHoverStyle: {
+				color: '#FFF'
+			},
+			itemHiddenStyle: {
+				color: '#606063'
+			}
+		},
+		credits: {
+			style: {
+				color: '#666'
+			}
+		},
+		labels: {
+			style: {
+				color: '#707073'
+			}
+		},
+
+		drilldown: {
+			activeAxisLabelStyle: {
+				color: '#F0F0F3'
+			},
+			activeDataLabelStyle: {
+				color: '#F0F0F3'
+			}
+		},
+
+		navigation: {
+			buttonOptions: {
+				symbolStroke: '#DDDDDD',
+				theme: {
+					fill: '#505053'
+				}
+			}
+		},
+
+		rangeSelector: {
+			buttonTheme: {
+				fill: '#505053',
+				stroke: '#000000',
+				style: {
+					color: '#CCC'
+				},
+				states: {
+					hover: {
+						fill: '#707073',
+						stroke: '#000000',
+						style: {
+							color: 'white'
+						}
+					},
+					select: {
+						fill: '#000003',
+						stroke: '#000000',
+						style: {
+							color: 'white'
+						}
+					}
+				}
+			},
+			inputBoxBorderColor: '#505053',
+			inputStyle: {
+				backgroundColor: '#333',
+				color: 'silver'
+			},
+			labelStyle: {
+				color: 'silver'
+			}
+		},
+
+		navigator: {
+			handles: {
+				backgroundColor: '#666',
+				borderColor: '#AAA'
+			},
+			outlineColor: '#CCC',
+			maskFill: 'rgba(255,255,255,0.1)',
+			series: {
+				color: '#7798BF',
+				lineColor: '#A6C7ED'
+			},
+			xAxis: {
+				gridLineColor: '#505053'
+			}
+		},
+
+		scrollbar: {
+			barBackgroundColor: '#808083',
+			barBorderColor: '#808083',
+			buttonArrowColor: '#CCC',
+			buttonBackgroundColor: '#606063',
+			buttonBorderColor: '#606063',
+			rifleColor: '#FFF',
+			trackBackgroundColor: '#404043',
+			trackBorderColor: '#404043'
+		},
+
+		legendBackgroundColor: 'rgba(0, 0, 0, 0.5)',
+		background2: '#505053',
+		dataLabelsColor: '#B0B0B3',
+		textColor: '#C0C0C0',
+		contrastTextColor: '#F0F0F3',
+		maskColor: 'rgba(255,255,255,0.3)'
+	};
+	Highcharts.setOptions(Highcharts.theme);
 
 
 </script>
