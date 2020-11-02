8bb48610b0c2c36310f2342d0cd8fbd761b327a1
ympimis <ympimis@gmail.com>
2020-11-02 10:49:20 +0700

    PR Revisi Title

--- resources/views/accounting_purchasing/purchase_requisition.blade.php
+++ resources/views/accounting_purchasing/purchase_requisition.blade.php
@@ -732,7 +732,7 @@
 										</div>
 
 										<div class="col-xs-2" style="padding:5px;">
-											<b>Kebutuhan / Hari</b>
+											<b>Kebutuhan</b>
 										</div>
 									</div>
 									
