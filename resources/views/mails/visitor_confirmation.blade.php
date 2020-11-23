4a95e078182bc01cbba6b92eb1692dcc215e223e
adityaagassi <adityaagassi@gmail.com>
2020-11-23 08:10:12 +0700

    Viisitor - Link Email

--- resources/views/mails/visitor_confirmation.blade.php
+++ resources/views/mails/visitor_confirmation.blade.php
@@ -43,7 +43,7 @@
 				</tbody>
 			</table>
 			<br>
-			<a href="{{ url("visitor_confirmation_manager") }}">Visitor Confirmation</a><br>
+			<a href="{{ url('visitor_confirmation_manager') }}">Visitor Confirmation</a><br>
 		</center>
 	</div>
 </body>
