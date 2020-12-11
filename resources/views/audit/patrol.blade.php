6cc6feab3ed171186c33ea55333150ef495ff7b1
adityaagassi <adityaagassi@gmail.com>
2020-12-11 09:50:44 +0700

    Patrol

--- resources/views/audit/patrol.blade.php
+++ resources/views/audit/patrol.blade.php
@@ -227,9 +227,8 @@
 					<div class="form-group">
 						<label>Pilih Kategori</label>
 						<select class="form-control select2" id="selectCategory" data-placeholder="Pilih Kategori..." style="width: 100%; font-size: 20px;">
+							<option value="S-Up And EHS Patrol Presdir">S-Up And EHS Patrol Presdir</option>
 							<option value="5S Patrol GM">5S Patrol GM</option>
-							<option value="5S Patrol Presdir">5S Patrol Presdir</option>
-							<option value="EHS Patrol">EHS Patrol</option>
 						</select>
 					</div>
 
