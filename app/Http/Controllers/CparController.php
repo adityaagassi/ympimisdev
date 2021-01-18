de7fe097ffaa529153738440c4311caa703887de
adityaagassi <adityaagassi@gmail.com>
2021-01-18 10:35:28 +0700

    Form ketidaksesuaian --

--- app/Http/Controllers/CparController.php
+++ app/Http/Controllers/CparController.php
@@ -217,7 +217,7 @@ class CparController extends Controller
               if ($dept->department == "Woodwind Instrument - Surface Treatment (WI-ST) Department") {
                 
                 if ($sec[1] == "Body Buffing-Barrel Process Section") {
-                  $sec[1] == "Buffing Key Process Section";
+                  $sec[1] = "Buffing Key Process Section";
                 }
 
                 $f = db::select("SELECT employee_id, name, position, section FROM employee_syncs where end_date is null and department = '".$dept->department."' and position = 'Foreman' and section='".$sec[1]."'");
