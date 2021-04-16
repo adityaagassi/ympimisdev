46201ef9c33424fcd4288815b14819c302e79b57
mkhamdank <gsyn153@gmail.com>
2021-04-16 07:51:12 +0700

    Office Clock 4 & 5

--- app/Http/Controllers/OfficeClockController.php
+++ app/Http/Controllers/OfficeClockController.php
@@ -315,6 +315,194 @@ class OfficeClockController extends Controller
     	}
     }
 
+    public function index4()
+    {
+    	date_default_timezone_set('Asia/Jakarta');
+		$date = date('m/d/Y H:i:s a', time());
+    	$dateTitle = date("l, d F Y", strtotime(date('Y-m-d')));
+    	$dayTitle = date("l", strtotime(date('Y-m-d')));
+    	return view('displays.office_clock4')->with('page', 'Clock')->with('head', 'Clock')->with('dateTitle',$dateTitle)->with('date',$date)->with('dayTitle',$dayTitle);
+    }
+
+    public function fetchVisitor4()
+    {
+    	$plc = PlcCounter::where('origin_group_code','visitor4')->first();
+    	$plc_lobby = PlcCounter::where('origin_group_code','visitor_lobby4')->first();
+
+    	$counter = $plc->plc_counter;
+    	$id_plc = $plc->id;
+
+    	$counter_lobby = $plc_lobby->plc_counter;
+    	$id_plc_lobby = $plc_lobby->id;
+
+    	$visitor = Visitor::where('destination','Office')->where('location','Security')->get();
+    	$visitor_lobby = Visitor::where('location','Lobby')->where('destination','Office')->get();
+
+    	$jumlahvisitor = count($visitor);
+    	$jumlahvisitor_lobby = count($visitor_lobby);
+
+    	if ($jumlahvisitor != $counter) {
+    		$visitors = DB::SELECT("
+    		SELECT
+				company,
+				name,
+				department,
+				location
+			FROM
+				`visitors`
+				JOIN employee_syncs ON employee_syncs.employee_id = employee 
+			WHERE
+				destination = 'Office' 
+				and location = 'Security'
+			ORDER BY
+				visitors.id DESC 
+				LIMIT 1
+			");
+
+			$plccounter = PlcCounter::find($id_plc);
+			$plccounter->plc_counter = $jumlahvisitor;
+			$plccounter->save();
+    	}
+
+    	if ($jumlahvisitor_lobby != $counter_lobby) {
+    		$visitors_lobby = DB::SELECT("
+    		SELECT
+				company,
+				name,
+				department,
+				location
+			FROM
+				`visitors`
+				JOIN employee_syncs ON employee_syncs.employee_id = employee 
+			WHERE
+				destination = 'Office' 
+				and location = 'Lobby'
+			ORDER BY
+				visitors.updated_at DESC 
+				LIMIT 1
+			");
+
+			$plccounter_lobby = PlcCounter::find($id_plc_lobby);
+			$plccounter_lobby->plc_counter = $jumlahvisitor_lobby;
+			$plccounter_lobby->save();
+    	}
+
+    	if (isset($visitors)) {
+    		$response = array(
+				'status' => true,
+				'visitors' => $visitors,
+				'visitors_lobby' => '',
+			);
+			return Response::json($response);
+    	}else if(isset($visitors_lobby)){
+    		$response = array(
+				'status' => true,
+				'visitors_lobby' => $visitors_lobby,
+				'visitors' => '',
+			);
+			return Response::json($response);
+    	}else{
+    		$response = array(
+				'status' => false
+			);
+			return Response::json($response);
+    	}
+    }
+
+    public function index5()
+    {
+    	date_default_timezone_set('Asia/Jakarta');
+		$date = date('m/d/Y H:i:s a', time());
+    	$dateTitle = date("l, d F Y", strtotime(date('Y-m-d')));
+    	$dayTitle = date("l", strtotime(date('Y-m-d')));
+    	return view('displays.office_clock5')->with('page', 'Clock')->with('head', 'Clock')->with('dateTitle',$dateTitle)->with('date',$date)->with('dayTitle',$dayTitle);
+    }
+
+    public function fetchVisitor5()
+    {
+    	$plc = PlcCounter::where('origin_group_code','visitor5')->first();
+    	$plc_lobby = PlcCounter::where('origin_group_code','visitor_lobby5')->first();
+
+    	$counter = $plc->plc_counter;
+    	$id_plc = $plc->id;
+
+    	$counter_lobby = $plc_lobby->plc_counter;
+    	$id_plc_lobby = $plc_lobby->id;
+
+    	$visitor = Visitor::where('destination','Office')->where('location','Security')->get();
+    	$visitor_lobby = Visitor::where('location','Lobby')->where('destination','Office')->get();
+
+    	$jumlahvisitor = count($visitor);
+    	$jumlahvisitor_lobby = count($visitor_lobby);
+
+    	if ($jumlahvisitor != $counter) {
+    		$visitors = DB::SELECT("
+    		SELECT
+				company,
+				name,
+				department,
+				location
+			FROM
+				`visitors`
+				JOIN employee_syncs ON employee_syncs.employee_id = employee 
+			WHERE
+				destination = 'Office' 
+				and location = 'Security'
+			ORDER BY
+				visitors.id DESC 
+				LIMIT 1
+			");
+
+			$plccounter = PlcCounter::find($id_plc);
+			$plccounter->plc_counter = $jumlahvisitor;
+			$plccounter->save();
+    	}
+
+    	if ($jumlahvisitor_lobby != $counter_lobby) {
+    		$visitors_lobby = DB::SELECT("
+    		SELECT
+				company,
+				name,
+				department,
+				location
+			FROM
+				`visitors`
+				JOIN employee_syncs ON employee_syncs.employee_id = employee 
+			WHERE
+				destination = 'Office' 
+				and location = 'Lobby'
+			ORDER BY
+				visitors.updated_at DESC 
+				LIMIT 1
+			");
+
+			$plccounter_lobby = PlcCounter::find($id_plc_lobby);
+			$plccounter_lobby->plc_counter = $jumlahvisitor_lobby;
+			$plccounter_lobby->save();
+    	}
+
+    	if (isset($visitors)) {
+    		$response = array(
+				'status' => true,
+				'visitors' => $visitors,
+				'visitors_lobby' => '',
+			);
+			return Response::json($response);
+    	}else if(isset($visitors_lobby)){
+    		$response = array(
+				'status' => true,
+				'visitors_lobby' => $visitors_lobby,
+				'visitors' => '',
+			);
+			return Response::json($response);
+    	}else{
+    		$response = array(
+				'status' => false
+			);
+			return Response::json($response);
+    	}
+    }
+
     public function guest_room()
     {
     	$apiKey = "GYhzWhGEkprqemup8Ps7VVts2jrt9kU8";
