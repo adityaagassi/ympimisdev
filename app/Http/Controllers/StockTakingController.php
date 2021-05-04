e7976f36937353f9824f4e280d3b6210aac67944
mkhamdank <gsyn153@gmail.com>
2021-05-04 07:52:35 +0700

    Error upload adjusment stocktaking

--- app/Http/Controllers/StockTakingController.php
+++ app/Http/Controllers/StockTakingController.php
@@ -2346,8 +2346,7 @@ class StockTakingController extends Controller{
 				$upload_text .= $this->writeDecimal(round($data->diff_abs,3), 13, "0");
 				$upload_text .= $this->writeStringReserve($data->cost_center, 10, "0");
 				$upload_text .= $this->writeString('', 10, " ");
-				$upload_text .= $this->writeDate('2021-02-27', "transfer");
-				// $upload_text .= $this->writeDate($calendar->date, "transfer");
+				$upload_text .= $this->writeDate($calendar->date, "transfer");
 				$upload_text .= $this->writeString('MB1C', 20, " ");
 				$upload_text .= $data->type;
 
