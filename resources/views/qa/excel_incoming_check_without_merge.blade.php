0152b52f4b028550ecb9d5357c5474fd1d24802c
mkhamdank <gsyn153@gmail.com>
2021-05-06 14:12:59 +0700

    Incoming Not Merge

--- resources/views/qa/excel_incoming_check_without_merge.blade.php
+++ resources/views/qa/excel_incoming_check_without_merge.blade.php
@@ -50,7 +50,7 @@
                 $ng_name = explode("_", $datas->ng_name);
                 $ng_qty = explode("_", $datas->ng_qty);
                 $status_ng = explode("_", $datas->status_ng);
-                if ($datas->ng_qty != null) {
+                if ($datas->note_ng != null) {
                     $note_ng = explode("_", $datas->note_ng);
                 }else{
                     $note_ng = "";
@@ -102,9 +102,13 @@
                             <td></td>
                             <td></td>
                             <td>{{$ng_qty[$i]}}</td>
-                        <?php }
-                        if (count($note_ng) > 0) {?>
-                            <td>{{$note_ng[$i]}}</td>
+                        <?php } ?>
+                        <?php if ($datas->note_ng != null) {?>
+                            <?php if (count($note_ng) > $i){ ?>
+                                <td>{{$note_ng[$i]}}</td>
+                            <?php }else{ ?>
+                                <td></td>
+                            <?php } ?>
                         <?php }else{ ?>
                             <td></td>
                         <?php } ?>
