c9badb60800bbe9c8c5aa3e8713c8ac2211037e6
mkhamdank <gsyn153@gmail.com>
2021-06-03 09:14:03 +0700

    date fix

--- resources/views/employees/report/employee_resume.blade.php
+++ resources/views/employees/report/employee_resume.blade.php
@@ -2,38 +2,38 @@
 @section('stylesheets')
 <link href="{{ url("css/jquery.gritter.css") }}" rel="stylesheet">
 <style type="text/css">
-	thead input {
-		width: 100%;
-		padding: 3px;
-		box-sizing: border-box;
-	}
-	thead>tr>th{
-		text-align:center;
-	}
-	tbody>tr>td{
-		text-align:center;
-	}
-	tfoot>tr>th{
-		text-align:center;
-	}
-	td:hover {
-		overflow: visible;
-	}
-	table.table-bordered{
-		border:1px solid black;
-	}
-	table.table-bordered > thead > tr > th{
-		border:1px solid black;
-	}
-	table.table-bordered > tbody > tr > td{
-		border:1px solid rgb(211,211,211);
-		padding-top: 0;
-		padding-bottom: 0;
-	}
-	table.table-bordered > tfoot > tr > th{
-		border:1px solid rgb(211,211,211);
-	}
-	#loading, #error { display: none; }
+thead input {
+	width: 100%;
+	padding: 3px;
+	box-sizing: border-box;
+}
+thead>tr>th{
+	text-align:center;
+}
+tbody>tr>td{
+	text-align:center;
+}
+tfoot>tr>th{
+	text-align:center;
+}
+td:hover {
+	overflow: visible;
+}
+table.table-bordered{
+	border:1px solid black;
+}
+table.table-bordered > thead > tr > th{
+	border:1px solid black;
+}
+table.table-bordered > tbody > tr > td{
+	border:1px solid rgb(211,211,211);
+	padding-top: 0;
+	padding-bottom: 0;
+}
+table.table-bordered > tfoot > tr > th{
+	border:1px solid rgb(211,211,211);
+}
+#loading, #error { display: none; }
 </style>
 @endsection
 
@@ -259,21 +259,17 @@
 
 	jQuery(document).ready(function() {
 		$('#datefrom').datepicker({
-			<?php $tgl_max = date('m-Y') ?>
 			format: "yyyy-mm",
 			startView: "months", 
 			minViewMode: "months",
 			autoclose: true,
-			endDate: '<?php echo $tgl_max ?>'
 
 		});
 		$('#dateto').datepicker({
-			<?php $tgl_max = date('m-Y') ?>
 			format: "yyyy-mm",
 			startView: "months", 
 			minViewMode: "months",
 			autoclose: true,
-			endDate: '<?php echo $tgl_max ?>'
 		});
 		$('.select2').select2();
 	});
