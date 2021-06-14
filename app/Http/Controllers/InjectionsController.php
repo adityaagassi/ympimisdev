9f39aac7d91a61ff66cea06b206c3bd7b77e79b2
mkhamdank <gsyn153@gmail.com>
2021-06-14 11:45:29 +0700

    Rev Block

--- app/Http/Controllers/InjectionsController.php
+++ app/Http/Controllers/InjectionsController.php
@@ -7319,6 +7319,7 @@ class InjectionsController extends Controller
                     AND injection_parts.deleted_at IS NULL 
                     AND injection_parts.remark = 'injection' 
                     AND injection_parts.part_code NOT LIKE '%MJ%' 
+                    AND injection_parts.part_code NOT LIKE '%BJ%' 
                 ) AS ng_name_kensa,
                 (
                 SELECT
@@ -7333,6 +7334,7 @@ class InjectionsController extends Controller
                     AND injection_parts.deleted_at IS NULL 
                     AND injection_parts.remark = 'injection' 
                     AND injection_parts.part_code NOT LIKE '%MJ%' 
+                    AND injection_parts.part_code NOT LIKE '%BJ%' 
                 ) AS ng_count_kensa 
             FROM
                 rc_kensa_initials
