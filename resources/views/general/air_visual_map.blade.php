7897bec2d891d1278f730ae7cf4e4ed907efeade
mkhamdank <gsyn153@gmail.com>
2021-06-16 16:56:44 +0700

    air visual

--- resources/views/general/air_visual_map.blade.php
+++ resources/views/general/air_visual_map.blade.php
@@ -44,7 +44,7 @@
 
 		td {
 			/*border-bottom: 1px solid white;*/
-			padding: 0px 5px 0px 5px;
+			padding: 0px 0px 0px 0px;
 			text-align:center;
 		}
 
@@ -313,13 +313,23 @@
 
 
 							table_content += '<td width="50%">';
-							table_content += '<div style="background-color: #605ca8; font-size: 25px; font-weight: bold;">'+node_name+'</div>';
-							table_content += '<div id="chart_co'+index+'" style="height: 180px; border: 1px solid #605ca8"></div>';
+							table_content += '<table width="100%"><tr><td colspan="2"><div style="background-color: #605ca8; font-size: 25px; font-weight: bold;">'+node_name+'</div></td></tr>';
+							table_content += '<tr><td width="80%"><div id="chart_co'+index+'" style="height: 180px; border: 1px solid #605ca8; width:100%"></div></td>';
+							table_content += '<td style="border: 1px solid #605ca8;" width="20%">';
+							table_content += '<b style="font-size: 18px">CO2 : '+result.last_data[index].co+' ppm</b><br>';
+							table_content += '<b style="font-size: 18px">Temp. : '+result.last_data[index].temperature+' &#8451;</b><br>';
+							table_content += '<b style="font-size: 18px">Humd. : '+result.last_data[index].humidity+' %</b>';
+							table_content += '</td></tr></table>';
 							table_content += '</td>';
 						} else {
 							table_content += '<td width="50%">';
-							table_content += '<div style="background-color: #605ca8; font-size: 25px; font-weight: bold;">'+node_name+'</div>';
-							table_content += '<div id="chart_co'+index+'" style="height: 180px; border: 1px solid #605ca8"></div>';
+							table_content += '<table width="100%"><tr><td colspan="2"><div style="background-color: #605ca8; font-size: 25px; font-weight: bold;">'+node_name+'</div></td></tr>';
+							table_content += '<tr><td width="80%"><div id="chart_co'+index+'" style="height: 180px; border: 1px solid #605ca8; width:100%"></div></td>';
+							table_content += '<td style="border: 1px solid #605ca8;" width="20%">';
+							table_content += '<b style="font-size: 18px">CO2 : '+result.last_data[index].co+' ppm</b><br>';
+							table_content += '<b style="font-size: 18px">Temp. : '+result.last_data[index].temperature+' &#8451;</b><br>';
+							table_content += '<b style="font-size: 18px">Humd. : '+result.last_data[index].humidity+' %</b>';
+							table_content += '</td></tr></table>';
 							table_content += '</td>';
 						}
 
