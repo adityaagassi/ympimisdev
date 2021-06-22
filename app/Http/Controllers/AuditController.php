e2b7c861178e3841183d69dc0d1cf3f029494994
mkhamdank <gsyn153@gmail.com>
2021-06-22 10:54:04 +0700

    Export Excel Audit All

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -1208,8 +1208,9 @@ public function detailPenanganan(Request $request){
 
       ob_clean();
 
-      Excel::create('Audit List '.$time, function($excel) use ($data){
+      Excel::create('Report '.$category.' '.$request->get('date'), function($excel) use ($data){
           $excel->sheet('Data', function($sheet) use ($data) {
+            $sheet->setAutoSize(true);
             return $sheet->loadView('audit.audit_excel', $data);
         });
       })->export('xlsx');
--- resources/views/audit/audit_excel.blade.php
+++ resources/views/audit/audit_excel.blade.php
@@ -1,26 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <style type="text/css">
+        table{
+            border: 2px solid black;
+            vertical-align: middle;
+        }
+        table > thead > tr > th{
+            border: 2px solid black;
+        }
+        table > tbody > tr > td{
+            border: 1px solid rgb(211,211,211);
+        }
+        table > tfoot > tr > th{
+            border: 1px solid rgb(211,211,211);
+        }
+    </style>
 </head>
 <body>
     @if(isset($detail) && count($detail) > 0)
     <table>
         <thead>
-            <tr>
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Kategori</th>
-                <th>Auditor</th>
-                <th>Lokasi</th>
-                <th>Auditee</th>
-                <th>Poin Kategori</th>
-                <th>Note</th>
-                <th>Foto</th>
-                <th>Status Penanganan</th>
-                <th>Tanggal Penanganan</th>
+            <tr style="background-color: #ddebf7; vertical-align: middle; ">
+                <th colspan="6" style="text-align: left;">PT. Yamaha Musical Products Indonesia</th>
             </tr>
+
+            <tr style="vertical-align: middle; ">
+                <th colspan="6" style="text-align: center;">
+                    @if($detail[0]->kategori == "Patrol Daily")
+                        Patrol Daily Shift 1 & Shift
+                    @elseif($detail[0]->kategori == "Patrol Covid")
+                        Patrol Covid
+                    @endif
+                </th>
+            </tr>
+
+            <tr></tr>
+            
+            <tr style="vertical-align: middle;">
+                <th colspan="2" style="text-align:left">Nama Petugas</th> 
+                <th>: {{ $detail[0]->auditor_name }}</th>
+            </tr>
+
+            <tr style="vertical-align: middle;">
+                <th colspan="2">Tanggal</th> 
+                <th>: <?php echo date('d-m-Y', strtotime($detail[0]->tanggal)) ?></th>
+            </tr>
+
+            <tr></tr>
         </thead>
         <tbody>
+
+            <tr>
+                <th style="border: 1px solid black">No</th>
+                <th style="border: 1px solid black">Lokasi</th>
+                <th style="border: 1px solid black">PIC</th>
+                <th style="border: 1px solid black">Poin Kategori</th>
+                <th style="border: 1px solid black">Note</th>
+                <th style="border: 1px solid black">Foto</th>
+            </tr>
+
             <?php 
                 $num = 1;
                 $amount=0; 
@@ -29,22 +68,12 @@
             @foreach($detail as $audit)
 
             <tr>
-                <td>{{ $num++ }}</td>
-                <td>{{ $audit->tanggal }}</td>
-                <td>{{ $audit->kategori }}</td>
-                <td>{{ $audit->auditor_name }}</td>
-                <td>{{ $audit->lokasi }}</td>
-                <td>{{ $audit->auditee_name }}</td>
-                <td>{{ $audit->point_judul }}</td>
-                <td>{{ $audit->note }}</td>
-                <td><img src="files/patrol/{{ $audit->foto }}" width="50"></td>
-
-                @if($audit->penanganan != "")
-                    <td>Close</td>
-                @else
-                    <td></td>
-                @endif
-                <td>{{ $audit->tanggal_penanganan }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left">{{ $num++ }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left">{{ $audit->lokasi }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left">{{ $audit->auditee_name }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left">{{ $audit->point_judul }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left">{{ $audit->note }}</td>
+                <td style="width: 200px;vertical-align: middle;text-align: left"><img src="files/patrol/{{ $audit->foto }}" width="200"></td>
             </tr>
 
             @endforeach
